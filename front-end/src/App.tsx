db5b5735e95ca092e4b18c780e696781acd96831
fabascoder <fabricialvez2@gmail.com>
2026-06-19 15:23:53 -0300

    Tentativa de criar rota, nao funciona no momento

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,32 +1,34 @@
-// import Header from './components/Header'
-// import MonthSection from './components/MonthSection'
-// import { months } from './data/posts'
-import Article from './components/pages/Article';
-
-import './App.css'
+import Header from "./components/Header";
+import MonthSection from "./components/MonthSection";
+import { months } from "./data/posts";
+import { Route, Routes, BrowserRouter, Router } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-   
+    <BrowserRouter>
       <div className="wr-page" id="top">
-      {/* <div className="wr-sheet">
-        <Header />
+        <div className="wr-sheet">
+          <Header />
 
-        <main className="wr-feed">
-          {months.map((month) => (
-            <MonthSection key={month.id} month={month} />
-          ))}
-        </main>
-        <footer className="wr-footer">
-          <span className="wr-footer-mark">Write.</span>
-          <span className="wr-footer-note">fim do arquivo</span>
-        </footer>
-      </div> */}
-      <Article />
-    </div>
-   
-    
-  )
+          <main className="wr-feed">
+            {months.map((month) => (
+              <Routes>
+                  <Route  path="/" element={<MonthSection key={month.id} month={month} />}/>
+              </Routes>
+                
+            ))}
+          </main>
+          <footer className="wr-footer">
+            <span className="wr-footer-mark">Write.</span>
+            <span className="wr-footer-note">fim do arquivo</span>
+          </footer>
+        </div>
+        {/* <Article /> */}
+      </div>
+    </BrowserRouter>
+      
+  );
 }
 
-export default App
+export default App;
--- front-end/src/components/MonthSection.tsx
+++ front-end/src/components/MonthSection.tsx
@@ -1,17 +1,25 @@
 import type { Month } from '../data/posts'
 import PostItem from './PostItem'
 import '../styles/MonthSection.css'
+import { Link, NavLink } from "react-router-dom";
+import Article from './pages/Article';
+
 
 export default function MonthSection({ month }: { month: Month }) {
   return (
+    
     <section className="wr-month">
       <h2 className="wr-month-title">{month.name}</h2>
       {/* ordered list: the timeline encodes real chronology */}
       <ol className="wr-timeline">
         {month.posts.map((post) => (
-          <PostItem key={post.id} post={post} />
+          <NavLink to="/article">
+             <PostItem key={post.id} post={post} />
+          </NavLink>
+         
         ))}
       </ol>
     </section>
+    
   )
 }
