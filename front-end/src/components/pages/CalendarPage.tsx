e3dc61e75c6c72fe5243093b771fddb6add5a747
fabascoder <fabricialvez2@gmail.com>
2026-09-22 13:07:08 -0300

    tentando arrumar aqui

--- front-end/src/components/pages/CalendarPage.tsx
+++ front-end/src/components/pages/CalendarPage.tsx
@@ -1,32 +1,22 @@
-import Header from "../layout/Header";
 import MonthSection from "../MonthSection";
 import { months } from "../../data/posts";
 import "../../styles/App.css";
-import { Footer } from "../layout/Footer";
 import { Head } from "../layout/Head";
 
 function CalendarPage() {
   return (
     <>
-    <Head title="Calendário" description="Essa é a ordem de postagem dos artigos"/>
-    <div className="wr-page" id="top">
-        <div className="wr-sheet">
-          <Header />
+      <Head
+        title="Calendário"
+        description="Essa é a ordem de postagem dos artigos"
+      />
 
-          <main className="wr-feed">
-            {months.map((month) => (
-              <MonthSection key={month.id} month={month} />
-            ))}
-          
-          </main>
-          <Footer />
-        </div>
-        {/* <Article /> */}
-      </div>
+      <main className="wr-feed">
+        {months.map((month) => (
+          <MonthSection key={month.id} month={month} />
+        ))}
+      </main>
     </>
-      
-
-      
   );
 }
 
